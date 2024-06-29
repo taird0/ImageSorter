157d0e80fb59e45be9ae6727b857fd55cc1bd972
taird0 <thomasaird0@gmail.com>
2024-06-29 13:50:53 +0100

    Finished off saveToDrive function

--- ImageSorter.py
+++ ImageSorter.py
@@ -1,11 +1,12 @@
-from PIL import Image, ImageOps
+from PIL import Image
 import argparse
 import os.path
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from google.oauth2.credentials import Credentials   
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+
 
 def checkColor(rgb):
     r, g, b = rgb
@@ -19,15 +20,51 @@ def checkColor(rgb):
     else:
         return False
     
-def saveToDrive(image_path, colorCode):
+def saveToDrive(image_path, folderID):
     creds = None
     
     #Access and refresh tokens stored in token.json - check if this already exists first
-    
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", scope='https://www.googleapis.com/auth/drive')
+    if not creds or not creds.valid:
+        #If user credentials are valid but expired refresh access token
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", scopes="https://www.googleapis.com/auth/drive"
+            )
+            creds = flow.run_local_server(port=0)
+        #Save credentials to json file
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("drive", "v3", credentials = creds)
+
+        #Upload file to specified folder
+
+        #splits the filepath - tail contains the filename and head rest of path
+        head, tail = os.path.split(image_path)
+        
+        file_metadata = {
+            "name": tail,
+            "parents": [folderID]
+        }
+
+        media = MediaFileUpload(image_path, mimetype='image/jpeg') 
+
+        file = service.files().create(body=file_metadata,
+                                      media_body = media,
+                                      fields='id').execute()
 
 def main(image_path):
 
-    colorCode = {'red': 'folder_path', 'blue': 'folder_path', 'green': 'folder_path'}
+    colorCode = {
+        'red': '1qiCQw66znHkb45JGsBk3i7HF9YhkMoPj',
+        'blue': '1Z3T7lj1dc6YhmUD0Uf0ZR_Ma_E8dNmv_',
+        'green': '1e2loTjljHEk4J78vn_l_uj5FeIzKcBQu'
+                }
 
     with Image.open(image_path) as im:
         
