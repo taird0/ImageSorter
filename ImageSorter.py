2be54bdbdd8d756ab0ec7131ebd4062df7206a92
taird0 <thomasaird0@gmail.com>
2024-07-02 11:25:45 +0100

    User can now pass a directory and app will sort all images in directory

--- ImageSorter.py
+++ ImageSorter.py
@@ -4,7 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
-import GoogleDriveAuthenticator
+from GoogleDriveAuthenticator import GoogleDriveAuthenticator as gda
 
 
 def checkColor(rgb):
@@ -29,7 +29,7 @@ def checkColor(rgb):
 
 def saveToDrive(image_path, folderID):
 
-    auth = GoogleDriveAuthenticator(client_secrets_file="credentials.json")
+    auth = gda(client_secrets_file="credentials.json")
 
     try:
         auth.authenticate()
@@ -59,15 +59,13 @@ def saveToDrive(image_path, folderID):
     except HttpError as error:
         print(f"Http Error: {error}")
 
-        
-def main(image_path):
-
+def process_image(image_path):
     #Dictionary containing GD folder ID's
     colorCode = {
         'red': '1qiCQw66znHkb45JGsBk3i7HF9YhkMoPj',
         'blue': '1Z3T7lj1dc6YhmUD0Uf0ZR_Ma_E8dNmv_',
         'green': '1e2loTjljHEk4J78vn_l_uj5FeIzKcBQu'
-                }
+    }
 
     with Image.open(image_path) as im:
         width, height = im.size
@@ -90,11 +88,22 @@ def main(image_path):
             saveToDrive(image_path, colorCode[color])
         else:
             print("The program could not identify a color.")
+        
+def main(image_path):
+    if image_path.lower().endswith('.jpg'):
+        process_image(image_path)
+    else:
+        for filename in os.listdir(image_path):
+            if filename.lower.endswith('.jpg'):
+                this_path = os.path.join(image_path, filename)
+                process_image(this_path)
+                print(f'{filename} saved to drive.')
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Save a JPG Image into Google Drive - Color coded")
-    parser.add_argument('input_image', help='Image to be sorted')
+    parser.add_argument('input_image', help='File/Directory to be sorted.')
     
     args = parser.parse_args()
     main(args.input_image)
